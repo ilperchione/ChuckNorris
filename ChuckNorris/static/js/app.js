091c861f3e88c26c33a04eeb3ecf8c2676db99e5
ilperchione <116425725+ilperchione@users.noreply.github.com>
2023-05-27 01:33:45 +0200

    terminato
    (da visualizzare con le dimensioni di Iphone 12 Pro)

--- ChuckNorris/static/js/app.js
+++ ChuckNorris/static/js/app.js
@@ -23,3 +23,53 @@ document.querySelector("a").addEventListener("click", function(e){
 		.catch(catchCallback)
 
 })
+
+fetch('https://api.chucknorris.io/jokes/categories')
+  .then(function(response) {
+    return response.json()
+  })
+  .then(data => {
+	const select = document.getElementById('selectCategory');
+	data.forEach(category => {
+	  const option = document.createElement('option');
+	  option.value = category;
+	  option.text = category;
+	  select.appendChild(option);
+	});
+  });
+
+document.querySelector("a").addEventListener("click", function(e){
+    e.preventDefault()
+    console.log("Cliccato")
+    let categoria = document.querySelector("select").value
+    let url = ""
+    if(categoria === "random"){
+        url = "https://api.chucknorris.io/jokes/random"
+    }else{
+        url = "https://api.chucknorris.io/jokes/random?category=" + categoria
+    }
+    fetch(url)
+    .then(function(data){
+        console.log("Fetch fatta")
+        return data.json()
+    })
+    .then(function(s){
+        console.log("Battuta inserita")
+        joke.innerHTML = s.value
+    })
+    .catch(function(){
+        console.log("Error!")
+    })
+})
+
+function copiaTesto() {
+    var testoDaCopiare = document.getElementById("joke").innerText;
+
+    navigator.clipboard.writeText(testoDaCopiare)
+      .then(function() {
+        alert("Testo copiato!");
+      })
+      .catch(function(error) {
+        console.error("Errore durante la copia del testo:", error);
+      });
+  }
